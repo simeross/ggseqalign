e20b9b9ec345cdb37d95d795fbd40b7e970b2924
Simeon <51403284+simeross@users.noreply.github.com>
2024-05-08 15:05:25 +0200

    simplified dependencies in example

--- R/plot_sequence_alignment.R
+++ R/plot_sequence_alignment.R
@@ -26,7 +26,9 @@
 #' pl2
 #'
 #' # Compatible with StringSets from Biostrings
-#' dna <- Biostrings::readDNAStringSet(system.file("extdata", "dm3_upstream2000.fa.gz",
+#' library(Biostrings)
+#' 
+#' dna <- readDNAStringSet(system.file("extdata", "dm3_upstream2000.fa.gz",
 #'                                     package="Biostrings"))
 #' # The entries dna[2:5] are identical
 #' q <- dna[2:4]
@@ -35,11 +37,11 @@
 #' pl3
 #'
 #' # Let's introduce some SNPs, insertions and deletions
-#' q[1] <- as(Biostrings::replaceLetterAt(q[[1]], c(5, 200, 400), "AGC"),
+#' q[1] <- as(replaceLetterAt(q[[1]], c(5, 200, 400), "AGC"),
 #'             "DNAStringSet")
 #' q[2] <- as(c(substr(q[[2]], 300, 1500), substr(q[[2]], 1800, 2000)),
 #'            "DNAStringSet")
-#' q[3] <- as(Biostrings::replaceAt(q[[3]], 1500,
+#' q[3] <- as(replaceAt(q[[3]], 1500,
 #'              paste(rep("A", 1000), collapse = "")),
 #'            "DNAStringSet")
 #' names(q) <- c("mismatches", "deletions", "insertion")
@@ -49,6 +51,8 @@
 #' pl4
 #'
 #' # Compatible with ggplot2 theming
+#' library(ggplot2)
+#' 
 #' pl4 +
 #'   ylab("Sequence variants") +
 #'   xlab("Length in bp") +
