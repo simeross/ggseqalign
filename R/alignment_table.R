a80858f929d8df873af1f3e350d3910605e5917f
Simeon <51403284+simeross@users.noreply.github.com>
2024-05-21 19:16:15 +0200

    compliance with Bioconductor guidelines

--- R/alignment_table.R
+++ R/alignment_table.R
@@ -29,107 +29,155 @@
 #' @export
 alignment_table <- function(query = XStringSet,
                             subject = XStringSet,
-                            ...){
-  if(is.null(names(query))){
-    names(query) <- paste0("query", 1:length(query))
-  }
-  if(any(is.na(names(query)))){
-    names(query)[is.na(names(query))] <- paste0("query",
-                                                which(is.na(names(query))))
-  }
-  if(is.null(names(subject))){
-    names(subject) <- "subject"
-  }
+                            ...) {
+    if (is.null(names(query))) {
+        names(query) <- paste0("query", seq_along(query))
+    }
+    if (any(is.na(names(query)))) {
+        names(query)[is.na(names(query))] <- paste0(
+            "query",
+            which(is.na(names(query)))
+        )
+    }
+    if (is.null(names(subject))) {
+        names(subject) <- "subject"
+    }
 
-  #Align all seqs to subject, also subject itself
-  all_sequences <- c(query, subject)
-  alig <- pairwiseAlignment(all_sequences, subject, ...)
+    # Align all seqs to subject, also subject itself
+    all_sequences <- c(query, subject)
+    alig <- pairwiseAlignment(all_sequences, subject, ...)
 
-  ranges <- alig@subject@range %>% 
-    as.data.frame()
-  p_starts <- alig@pattern@range %>% 
-    as.data.frame() %>% 
-    .$start
+    ranges <- alig@subject@range %>%
+        as.data.frame()
+    p_starts <- alig@pattern@range %>%
+        as.data.frame() %>%
+        .$start
 
-  #Extract insertions first to use in "end" adjustment for mismatches
-  insertions <- alig@subject@indel %>% as.data.frame() %>%
-    as_tibble() %>%
-    left_join(tibble(group = 1:length(names(all_sequences)),
-                     PatternName = names(all_sequences)), by = "group") %>%
-    mutate(PatternName = factor(PatternName,
-                                levels = c(names(subject),
-                                           names(query))))
-  #Extract mismatches, add empty rows if none detected
-  mismatches <- alig %>%
-    mismatchTable() %>%
-    as_tibble()
-  if(nrow(mismatches) == 0){
-    mismatches <- add_row(mismatches,
-                          PatternId = 1:length(names(all_sequences)))
-  }
-  mismatches <- full_join(mismatches,
-            tibble(PatternId = 1:length(names(all_sequences)),
-                     PatternName = names(all_sequences),
-                     PatternLength = width(all_sequences),
-                     ranges,
-                   alig_start = p_starts), by = join_by(PatternId)) %>%
-    # start and end need to be adjusted so that each character is of width 1
-    # centered on its number, e.g. character 4 is from 3.5-4.5
-    mutate(start = start - 0.5,
-           end = end + 0.5)
-  # include the reverse mismatches against the subject to show the original
-  # characters on the subject
-  mismatches_ref <- dplyr::select(mismatches, contains("Subject")) %>%
-    filter(!is.na(SubjectSubstring)) %>%
-    dplyr::rename(PatternSubstring = "SubjectSubstring") %>%
-    bind_cols(
-      dplyr::select(mismatches, PatternId, PatternName,
-             PatternLength, start, end, width) %>%
-        filter(PatternName == names(subject))
-    )
-  #combine with mismatches
-  mismatches <- bind_rows(mismatches, mismatches_ref) %>%
-    mutate(PatternName = factor(PatternName,
-                                levels = c(names(subject),
-                                           names(query)))) %>%
-    # adjust end to extend over subject
-    mutate(end = ifelse(end < (PatternLength-sum(insertions$width)+start),
-                        (PatternLength-sum(insertions$width)+start), end)) %>%
-    # adjust start to extend before subject
-    mutate(start = ifelse(alig_start > 1, start-alig_start, start)) %>% 
-    select(-alig_start) %>% 
-    unique() %>%
-    mutate(feature = "mismatch")
+    # Extract insertions first to use in "end" adjustment for mismatches
+    insertions <- alig@subject@indel %>%
+        as.data.frame() %>%
+        as_tibble() %>%
+        left_join(tibble(
+            group = seq_along(names(all_sequences)),
+            PatternName = names(all_sequences)
+        ), by = "group") %>%
+        mutate(PatternName = factor(PatternName,
+            levels = c(
+                names(subject),
+                names(query)
+            )
+        ))
+    # Extract mismatches, add empty rows if none detected
+    mismatches <- alig %>%
+        mismatchTable() %>%
+        as_tibble()
+    if (nrow(mismatches) == 0) {
+        mismatches <- add_row(mismatches,
+            PatternId = seq_along(names(all_sequences))
+        )
+    }
+    mismatches <- full_join(
+        mismatches,
+        tibble(
+            PatternId = seq_along(names(all_sequences)),
+            PatternName = names(all_sequences),
+            PatternLength = width(all_sequences),
+            ranges,
+            alig_start = p_starts
+        ),
+        by = join_by(PatternId)
+    ) %>%
+        # start and end need to be adjusted so that each character is of width 1
+        # centered on its number, e.g. character 4 is from 3.5-4.5
+        mutate(
+            start = start - 0.5,
+            end = end + 0.5
+        )
+    # include the reverse mismatches against the subject to show the original
+    # characters on the subject
+    mismatches_ref <-
+        dplyr::select(mismatches, contains("Subject")) %>%
+        filter(!is.na(SubjectSubstring)) %>%
+        dplyr::rename(PatternSubstring = "SubjectSubstring") %>%
+        bind_cols(
+            dplyr::select(
+                mismatches,
+                PatternId,
+                PatternName,
+                PatternLength,
+                start,
+                end,
+                width
+            ) %>%
+                filter(PatternName == names(subject))
+        )
+    # combine with mismatches
+    mismatches <- bind_rows(mismatches, mismatches_ref) %>%
+        mutate(PatternName = factor(PatternName,
+            levels = c(
+                names(subject),
+                names(query)
+            )
+        )) %>%
+        # adjust end to extend over subject
+        mutate(end = ifelse(
+            end < (PatternLength - sum(insertions$width) + start),
+            (PatternLength - sum(insertions$width) + start),
+            end
+        )) %>%
+        # adjust start to extend before subject
+        mutate(start = ifelse(alig_start > 1, start - alig_start, start)) %>%
+        select(-alig_start) %>%
+        unique() %>%
+        mutate(feature = "mismatch")
 
-  # Extract deletions
-  deletions <- alig@pattern@indel %>% as.data.frame() %>%
-    as_tibble() %>%
-    left_join(tibble(group = 1:length(names(all_sequences)),
-                     PatternName = names(all_sequences)), by = "group") %>%
-    mutate(PatternName = factor(PatternName,
-                                levels = c(names(subject),
-                                           names(query)))) %>%
-    # start and end need to be adjusted so that each character deletion is of
-    # width 1 centered on its number, e.g. character deletion 4 is from 3.5-4.5
-    mutate(start = start - 0.5,
-           end = end + 0.5,
-           feature = "deletion")
+    # Extract deletions
+    deletions <- alig@pattern@indel %>%
+        as.data.frame() %>%
+        as_tibble() %>%
+        left_join(tibble(
+            group = seq_along(names(all_sequences)),
+            PatternName = names(all_sequences)
+        ), by = "group") %>%
+        mutate(PatternName = factor(PatternName,
+            levels = c(
+                names(subject),
+                names(query)
+            )
+        )) %>%
+        # start and end need to be adjusted so that each character 
+        # deletion is of width 1 centered on its number, e.g. 
+        # character deletion 4 is from 3.5-4.5
+        mutate(
+            start = start - 0.5,
+            end = end + 0.5,
+            feature = "deletion"
+        )
 
 
     # Careful, subject@indel is relative to the query, unlike pattern@indel,
     # Adjust by merging with mismatch tibble and adding start.
     insertions <- rename(insertions, start_ins = "start") %>%
-    left_join(dplyr::select(mismatches, start, PatternName),
-              by = join_by(PatternName)) %>%
-    mutate(end = end + start,
-           start = start_ins-1+start,
-           feature = "insertion")
+        left_join(dplyr::select(mismatches, start, PatternName),
+            by = join_by(PatternName)
+        ) %>%
+        mutate(
+            end = end + start,
+            start = start_ins - 1 + start,
+            feature = "insertion"
+        )
 
-    #Combine mismatches, deletions and insertions into one tbl
-    out <- bind_rows(mismatches,
-              dplyr::select(insertions, start, end, width,
-                            PatternName, feature),
-              dplyr::select(deletions, start, end, width,
-                            PatternName, feature))
-  return(out)
+    # Combine mismatches, deletions and insertions into one tbl
+    out <- bind_rows(
+        mismatches,
+        dplyr::select(
+            insertions, start, end, width,
+            PatternName, feature
+        ),
+        dplyr::select(
+            deletions, start, end, width,
+            PatternName, feature
+        )
+    )
+    return(out)
 }
--- R/plot_sequence_alignment.R
+++ R/plot_sequence_alignment.R
@@ -27,9 +27,10 @@
 #'
 #' # Compatible with StringSets from Biostrings
 #' library(Biostrings)
-#' 
+#'
 #' dna <- readDNAStringSet(system.file("extdata", "dm3_upstream2000.fa.gz",
-#'                                     package="Biostrings"))
+#'     package = "Biostrings"
+#' ))
 #' # The entries dna[2:5] are identical
 #' q <- dna[2:4]
 #' s <- dna[5]
@@ -37,13 +38,21 @@
 #' pl3
 #'
 #' # Let's introduce some SNPs, insertions and deletions
-#' q[1] <- as(replaceLetterAt(q[[1]], c(5, 200, 400), "AGC"),
-#'             "DNAStringSet")
-#' q[2] <- as(c(substr(q[[2]], 300, 1500), substr(q[[2]], 1800, 2000)),
-#'            "DNAStringSet")
-#' q[3] <- as(replaceAt(q[[3]], 1500,
-#'              paste(rep("A", 1000), collapse = "")),
-#'            "DNAStringSet")
+#' q[1] <- as(
+#'     replaceLetterAt(q[[1]], c(5, 200, 400), "AGC"),
+#'     "DNAStringSet"
+#' )
+#' q[2] <- as(
+#'     c(substr(q[[2]], 300, 1500), substr(q[[2]], 1800, 2000)),
+#'     "DNAStringSet"
+#' )
+#' q[3] <- as(
+#'     replaceAt(
+#'         q[[3]], 1500,
+#'         paste(rep("A", 1000), collapse = "")
+#'     ),
+#'     "DNAStringSet"
+#' )
 #' names(q) <- c("mismatches", "deletions", "insertion")
 #' names(s) <- substr(names(s)[1], 1, 34)
 #'
@@ -52,79 +61,112 @@
 #'
 #' # Compatible with ggplot2 theming
 #' library(ggplot2)
-#' 
-#' pl4 +
-#'   ylab("Sequence variants") +
-#'   xlab("Length in bp") +
-#'   scale_color_viridis_d() +
-#'   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-#'         axis.title = element_text())
 #'
+#' pl4 +
+#'     ylab("Sequence variants") +
+#'     xlab("Length in bp") +
+#'     scale_color_viridis_d() +
+#'     theme(
+#'         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+#'         axis.title = element_text()
+#'     )
 #'
 #' @import ggplot2 dplyr
 #'
 #' @export
 plot_sequence_alignment <- function(alignment_tbl =
-                                      alignment_table(query, subject),
+                                        alignment_table(query, subject),
                                     insertion_color = "#21918c",
-                                    hide_mismatches = FALSE){
-  # line themeing
-  lt <- "dashed"
-  co <- "grey"
-  ins_w <- 2
-  u <- "unique"
+                                    hide_mismatches = FALSE) {
+    # line themeing
+    lt <- "dashed"
+    co <- "grey"
+    ins_w <- 2
+    u <- "unique"
 
-  # plot lines representing strings
-  pl <- ggplot(filter(alignment_tbl, feature == "mismatch"),
-               aes(y = PatternName)) +
-    geom_linerange(aes(xmin = start, xmax = end), stat = "unique") +
-    # plot white lines representing deletions over the original lines,
-    # causing the illusion of a gap.
-    geom_linerange(data = filter(alignment_tbl, feature == "deletion"),
-                 aes(xmin = start, xmax = end),
-                 linewidth = ins_w, color = "white", stat = u) +
-    # plot insertions centered on the point of insertion with dashed lines
-    # indicating the point of insertion
-    # This is the actual insertion
-    geom_linerange(data = filter(alignment_tbl, feature == "insertion"),
-                 aes(xmin = start-width/2, xmax = end-width/2,
-                     y = as.numeric(PatternName)+0.4),
-                 color = insertion_color, stat = u) +
-    # This is the left leg of the indicator of insertion point
-    geom_linerange(data = filter(alignment_tbl, feature == "insertion"),
-                   aes(x = start-0.1, ymin = PatternName,
-                       ymax = as.numeric(PatternName)+0.3),
-                   linetype = lt , stat = u, color = co) +
-    # This is the right leg of the indicator of insertion point
-    geom_linerange(data = filter(alignment_tbl, feature == "insertion"),
-                   aes(x = start+0.1, ymin = PatternName,
-                       ymax = as.numeric(PatternName)+0.3),
-                   linetype = lt , stat = u, color = co) +
-    # This is the left arm of the indicator of insertion point
-    geom_segment(data = filter(alignment_tbl, feature == "insertion"),
-                 aes(x = start-0.1, xend = start-width/2,
-                       y = as.numeric(PatternName)+0.3,
-                       yend = as.numeric(PatternName)+0.4),
-                 linetype = lt , stat = u, color = co) +
-    # This is the right arm of the indicator of insertion poin
-    geom_segment(data = filter(alignment_tbl, feature == "insertion"),
-                 aes(x = start+0.1, xend = end-width/2,
-                     y = as.numeric(PatternName)+0.3,
-                     yend = as.numeric(PatternName)+0.4),
-                 linetype = lt , stat = u, color = co) +
-    theme_classic() +
-    theme(axis.title = element_blank(),
-          legend.position = "none")
+    # plot lines representing strings
+    pl <- ggplot(
+        filter(alignment_tbl, feature == "mismatch"),
+        aes(y = PatternName)
+    ) +
+        geom_linerange(aes(xmin = start, xmax = end), stat = "unique") +
+        # plot white lines representing deletions over the original lines,
+        # causing the illusion of a gap.
+        geom_linerange(
+            data = filter(alignment_tbl, feature == "deletion"),
+            aes(xmin = start, xmax = end),
+            linewidth = ins_w, color = "white", stat = u
+        ) +
+        # plot insertions centered on the point of insertion with dashed lines
+        # indicating the point of insertion
+        # This is the actual insertion
+        geom_linerange(
+            data = filter(alignment_tbl, feature == "insertion"),
+            aes(
+                xmin = start - width / 2, xmax = end - width / 2,
+                y = as.numeric(PatternName) + 0.4
+            ),
+            color = insertion_color, stat = u
+        ) +
+        # This is the left leg of the indicator of insertion point
+        geom_linerange(
+            data = filter(alignment_tbl, feature == "insertion"),
+            aes(
+                x = start - 0.1, ymin = PatternName,
+                ymax = as.numeric(PatternName) + 0.3
+            ),
+            linetype = lt, stat = u, color = co
+        ) +
+        # This is the right leg of the indicator of insertion point
+        geom_linerange(
+            data = filter(alignment_tbl, feature == "insertion"),
+            aes(
+                x = start + 0.1, ymin = PatternName,
+                ymax = as.numeric(PatternName) + 0.3
+            ),
+            linetype = lt, stat = u, color = co
+        ) +
+        # This is the left arm of the indicator of insertion point
+        geom_segment(
+            data = filter(alignment_tbl, feature == "insertion"),
+            aes(
+                x = start - 0.1, xend = start - width / 2,
+                y = as.numeric(PatternName) + 0.3,
+                yend = as.numeric(PatternName) + 0.4
+            ),
+            linetype = lt, stat = u, color = co
+        ) +
+        # This is the right arm of the indicator of insertion poin
+        geom_segment(
+            data = filter(alignment_tbl, feature == "insertion"),
+            aes(
+                x = start + 0.1, xend = end - width / 2,
+                y = as.numeric(PatternName) + 0.3,
+                yend = as.numeric(PatternName) + 0.4
+            ),
+            linetype = lt, stat = u, color = co
+        ) +
+        theme_classic() +
+        theme(
+            axis.title = element_blank(),
+            legend.position = "none"
+        )
 
-  # Add mismatches as point plot if any exist
-  if(any(!is.na(alignment_tbl$PatternSubstring)) &
-     !hide_mismatches){
-    pl <- pl + geom_point(aes(x = SubjectStart,
-                                          color = PatternSubstring)) +
-      geom_text(aes(x = SubjectStart, label = PatternSubstring,
-                    color = PatternSubstring),
-                nudge_y = 0.2, hjust = 0.5)
-  }
+    # Add mismatches as point plot if any exist
+    if (any(!is.na(alignment_tbl$PatternSubstring)) &
+        !hide_mismatches) {
+        pl <- pl + geom_point(aes(
+            x = SubjectStart,
+            color = PatternSubstring
+        )) +
+            geom_text(
+                aes(
+                    x = SubjectStart, label = PatternSubstring,
+                    color = PatternSubstring
+                ),
+                nudge_y = 0.2, hjust = 0.5
+            )
+    }
 
-  return(pl)
+    return(pl)
 }
